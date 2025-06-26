f619741c47f0f9993fd6491e6cbcc5e0c471a6c4
Oleh Basystyi <basystyo@gmail.com>
2025-06-26 14:51:06 +0300

    add undistortion to COLMAP script

--- scripts/colmap_sparse.sh
+++ scripts/colmap_sparse.sh
@@ -33,11 +33,22 @@ colmap mapper \
     --Mapper.ba_global_function_tolerance=0.000001 \
     --Mapper.ba_use_gpu 1
 
-echo "[INFO] Converting model to PLY..."
-colmap model_converter \
+echo "[INFO] Undistorting images and converting to pinhole model..."
+colmap image_undistorter \
+    --image_path images \
     --input_path sparse/0 \
-    --output_path points3D.ply \
+    --output_path undistorted \
+    --output_type COLMAP \
+    --max_image_size 2000
+
+echo "[INFO] Converting undistorted model to PLY..."
+colmap model_converter \
+    --input_path undistorted/sparse \
+    --output_path undistorted/points3D.ply \
     --output_type PLY
 
-echo "[DONE] GLOMAP sparse reconstruction complete. Output saved to $PROJECT_NAME/points3D.ply"
+rm -r sparse
+mv undistorted/sparse .
+
+echo "[DONE] COLMAP sparse reconstruction complete. Output saved to $PROJECT_NAME/points3D.ply"
 echo "[INFO] Total time taken: $SECONDS seconds"
